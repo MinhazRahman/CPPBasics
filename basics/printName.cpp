57b329d92711310e7a057c3d8db842aa130fc94a
Mohammed Rahman <minhaz357@gmail.com>
2023-02-13 04:48:02 -0500

    insertion and extraction operators

--- basics/printName.cpp
+++ basics/printName.cpp
@@ -4,9 +4,21 @@ using namespace std;
 int main()
 {
   string name;
+  int age;
+  string country;
 
   cout << "Please, enter your name:" << endl;
-  getline(cin, name);
+  getline(cin, name); // takes a string until we press enter key
+
+  cout << "Please, enter your age:" << endl;
+  cin >> age; // takes an int type data
+
+  cout << "Please, enter your country:" << endl;
+  cin >> country; // takes a string without the space
+
   cout << "Your name is: " << name << endl;
+  cout << "Your age is: " << age << endl;
+  cout << "Your country is: " << country << endl;
+
   return 0;
 }
